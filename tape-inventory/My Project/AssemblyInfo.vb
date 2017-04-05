3575770695fccd94b93bea7e5f60474d9533e3dd
richard.jf.fisk <richard.jf.fisk@gmail.com>
2017-04-05 09:51:44 -0700

    Add project files.

---
+++ tape-inventory/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("tape-inventory")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Boxx Technologies, Inc.")>
+<Assembly: AssemblyProduct("tape-inventory")>
+<Assembly: AssemblyCopyright("Copyright © Boxx Technologies, Inc. 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("123f2480-edc5-4460-9160-b96b43644b39")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
